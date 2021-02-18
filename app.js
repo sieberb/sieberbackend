6b7f5a900226fd3884e04ff767b468ab7f44987c
Sieber <SieberBulteel@gmail.com>
2021-02-18 15:20:55 +0100

    backend bijna af

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 80
 
+// meer open dan standaard (naar http https toe)
+var cors = require('cors')
+app.use(cors())
+
+// extra code van wesley op Slack
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json())
+
 const { Pool, Client } = require('pg')
 const pool = new Pool({
     user: 'wjajqjouwoxhrb',
@@ -26,6 +40,36 @@ app.get('/lijst', (req, res) => {
         res.send(JSON.stringify(res2.rows));
     })
 })
+
+// GET SNACKS
+app.get('/frituursnacks', (req, res) => {
+    // QUERY UITVOEREN
+    pool.query('SELECT * FROM frituursnacks', (err, res2) => {
+        // console.log(res2);
+        res.send(JSON.stringify(res2.rows));
+    })
+})
+
+// ADD SNACK MY WAY
+app.post('/frituursnacks/nieuw', (req, res) => {
+    // console.log(req.body);
+    // hier INSERT INTO insteken
+    pool.query("INSERT INTO frituursnacks(naam, frituurtijd, vegetarisch) VALUES('"+req.body.naam + "', '" + req.body.frituurtijd + "', '" + req.body.vegetarisch+"')", (err, res3) => {
+    });
+    res.send('De ' + req.body.naam + ' werd succesvol toegevoegd')
+})
+
+// ADD SNACK MELISSA'S WAY - template literals
+/* app.post('/frituursnacks/nieuw', (req, res) => {
+    // console.log(req.body);
+    pool.query(`INSERT INTO frituursnacks(naam,frituurtijd,vegetarisch) VALUES('${req.body.naam}','${req.body.frituurtijd}','${req.body.vegetarisch}')`,
+        (error, results) => {
+            if (error) {
+                throw error;
+            }
+            res.send('De ' + req.body.naam + ' werd succesvol toegevoegd');
+        });
+}); */
 
 app.get('/test', (req, res) => {
     let arr = ['wesley', 'laura', 'brent'];
